382ae6afbdb8ca01768daf2f2353880b58e54948
rpol-recart <68892604+rpol-recart@users.noreply.github.com>
2023-04-20 08:41:50 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,3 +2,4 @@ https://www.evernote.com/shard/s44/sh/ce2444f6-b681-b05b-cf27-ba938f0c9330/tTD4z
 https://www.evernote.com/shard/s44/sh/9bae4e2c-13d2-2934-bbd5-fe941a797f82/5jiBs9qLEjU8GKLUpdX0fDZOQcRQlDaGP_8MD7w2TmhuB-IfKyECWuwuxA
 https://www.evernote.com/shard/s44/sh/ce2444f6-b681-b05b-cf27-ba938f0c9330/mzJ2KJevPSNJ9ySBiumEuPTCRBx09hWO7zizG0XEJpVvspmltr7oNDyLHQ
 https://www.evernote.com/shard/s44/sh/8f5ff5bf-8527-1552-409e-dcd64dc6607d/xzzCDEAO70xW0gRh6VWFIBSBtvvK0L6Dwp94bDaTlBnniwM2i1quZ_mp0Q
+https://www.evernote.com/shard/s44/sh/99212a35-81d0-188c-6e36-3136df21cc62/4VmG6Tr8HqNLC8l0CMMtyKK1QNr7rbtV1OsTRakoHds4lhzdpFGDyMMLqQ
